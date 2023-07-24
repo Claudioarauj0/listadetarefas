07373a117e80f949954ceea56a137706a75fabd3
Claudioarauj0 <claudinhosas@gmail.com>
2023-07-24 15:30:02 -0300

    inicio do projeto

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Adicione uma tarefa',
+                hintText: "estudar",
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('+'))
+          ],
+        ),
+      ),
+    );
+  }
+}
